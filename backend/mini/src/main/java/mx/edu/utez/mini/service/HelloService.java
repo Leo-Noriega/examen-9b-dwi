0a7d9a609abc6e6b229d33d7f4fb12645de60a6a
Leo-Noriega <leonoriega100@gmail.com>
2025-05-06 15:58:50 -0600

    fix: change backend

--- backend/mini/src/main/java/mx/edu/utez/mini/service/HelloService.java
+++ backend/mini/src/main/java/mx/edu/utez/mini/service/HelloService.java
@@ -1,5 +1,8 @@
 package mx.edu.utez.mini.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelloService {
     public String helloWorld() {
         return "Hola desde mi backend";
